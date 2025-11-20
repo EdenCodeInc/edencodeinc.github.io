c0418d88d6028bdca2b9a7acd58d65e8fc842651
Everett <EverettYou@users.noreply.github.com>
2025-11-20 02:46:03 -0800

    debug

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,9 +5,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/', // Organization site (edencodeinc.github.io)
-  build: {
-    outDir: 'build',
-  },
   plugins: [react()],
   resolve: {
     alias: {
